3143983e97998a18da69d23aa97f876fe59f0721
Kristýna Antošová <antosovkr@natur.cuni.cz>
2022-12-10 21:30:46 +0100

    hotovo

--- 2_du.py
+++ 2_du.py
@@ -1,112 +1,76 @@
 import csv
 import copy
-
-"""# Otevřeme a přečteme soubor Data_prutok.csv
-with open("Data_prutok.csv", encoding="utf-8", newline='') as f:
-	reader = csv.reader(f, delimiter=",")
-	for row in reader:
-		print(row)
-with open("Data_prutok.csv", encoding="utf-8", newline='') as f:
-	reader = csv.reader(f, delimiter=",")
-	for row in range(7):
-		(prutoky) = (next(reader)[3])
-		print (prutoky)
-prutoky_list = []
-prutoky_list.append(prutoky)
-print(prutoky_list)
-#def průměr_7(součet_průtoků, pocet_prutoků):"""
+import sys
 
 suma_tyden = 0
 suma_rok = 0
 pocitadlo_radku = 1
-
-with open("Data_prutok.csv", encoding="utf-8", newline='') as f:
-	reader = csv.reader(f, delimiter=",")
-	for row in reader:
-		suma_tyden += float(row[3])
-		if pocitadlo_radku % 7 == 0:
-			suma_tyden = round(suma_tyden/7,4)
-			print (suma_tyden)
-			suma_tyden = 0
-		pocitadlo_radku+=1
+radek = [] 
 
 novy_rok = False
 stary_rok = 0
 pocitadlo_dnu_v_roku = 1
-radek = []
 
-with open("Data_prutok.csv", encoding="utf-8", newline='') as f:
-	reader = csv.reader(f, delimiter=",")
-	for row in reader:
-		stary_rok = int(row[2].split(".")[2])
-		radek = copy.deepcopy(row)
-		break
+f = open('out_tydenni.csv', 'w',newline='')                             #Vytvoření csv výstupu se sedmidenními průtoky řeky
+writer_tydenni = csv.writer(f)
 
-f = open('out_rocni.csv', 'w',newline='')
+try:                                                                       
+	with open("Data_prutok.csv", encoding="utf-8", newline='') as f:     
+		reader = csv.reader(f, delimiter=",")
+		for row in reader:
+			if pocitadlo_radku % 7 == 1:                                # zápis řádku prvního dne v týdnu do proměnné radek 
+				radek = copy.deepcopy(row)
+			if pocitadlo_radku % 7 == 0:
+				radek[3] = round(suma_tyden/7,4)						#zapis průměrného průtoku do proměnné radek
+				writer_tydenni.writerow(radek)                          #zápis proměnné radek do csv souboru
+				suma_tyden = 0
+			if float(row[3]) < 0:
+				print("Data obsahují zápornou hodnotu průtoku.")
+				sys.exit()
+			suma_tyden += float(row[3])
+			pocitadlo_radku+=1
 
-writer_rok = csv.writer(f)
+	if pocitadlo_radku % 7 != 0:                                       #poslední průměr program spočítá z tolika dat, kolik je k dispozici
+		radek[3] = round(suma_tyden/(pocitadlo_radku % 7),4)
+		writer_tydenni.writerow(radek)
+
+	f.close()
 
 
-print("průmery za rok:")
-with open("Data_prutok.csv", encoding="utf-8", newline='') as f:
-	reader = csv.reader(f, delimiter=",")
-	for row in reader:
-		rok = int(row[2].split(".")[2])
-		if stary_rok == rok: 
-			novy_rok = False
-		else: 
-			novy_rok = True
-		if novy_rok == True:
-			radek[3] = round(suma_rok/pocitadlo_dnu_v_roku,4)
-			pocitadlo_dnu_v_roku = 0
-			suma_rok = 0
-			stary_rok = rok
-			datum = copy.deepcopy(radek[2].split("."))
-			datum[2] = str(radek[2].split(".")[2])
-			datum = ".".join(datum)
-			radek[2] = copy.deepcopy(datum)
-			writer_rok.writerow(radek)
+	with open("Data_prutok.csv", encoding="utf-8", newline='') as f:   # 39 - 44 - slouží ke zjištění, kterým rokem začíná dataset
+		reader = csv.reader(f, delimiter=",")
+		for row in reader:												
+			stary_rok = int(row[2].split(".")[2])
 			radek = copy.deepcopy(row)
-		suma_rok += float(row[3])
-		pocitadlo_dnu_v_roku += 1
+			break
 
-radek[3] = round(suma_rok/pocitadlo_dnu_v_roku,4)
-datum = copy.deepcopy(radek[2].split("."))
-datum[2] = str(int(radek[2].split(".")[2]) + 1)
-datum = ".".join(datum)
-radek[2] = copy.deepcopy(datum)
+	f = open('out_rocni.csv', 'w',newline='')							#Vytvoření csv výstupu se ročními průtoky řeky
+	writer_rok = csv.writer(f)
 
-writer_rok.writerow(radek)
+	with open("Data_prutok.csv", encoding="utf-8", newline='') as f:
+		reader = csv.reader(f, delimiter=",")
+		for row in reader:
+			rok = int(row[2].split(".")[2])
+			if stary_rok == rok: 
+				novy_rok = False
+			else: 
+				novy_rok = True
+			if novy_rok == True:                                        #jestliže se změní rok v datech zapíše se řádek do csv souboru
+				radek[3] = round(suma_rok/pocitadlo_dnu_v_roku,4)
+				pocitadlo_dnu_v_roku = 0
+				suma_rok = 0
+				stary_rok = rok
+				writer_rok.writerow(radek)
+				radek = copy.deepcopy(row)
+			suma_rok += float(row[3])
+			pocitadlo_dnu_v_roku += 1
 
-f.close()
+	radek[3] = round(suma_rok/pocitadlo_dnu_v_roku,4)                   #zapsání posledního roku v datasetu
+	writer_rok.writerow(radek)
 
-"""
+	f.close()
 
-f = open('out_rocni', 'w')
-
-writer_rok = csv.writer(f)
-
-writer_rok.writerow(radek)
-
-f.close()
-
-Otevřeme si soubor `parkoviste.csv` pro čtení a `pr.csv` pro zápis (parametr `"w"`)
-# \ na konci prvního řádku je proto, aby šel druhý open napsat na nový řádek
-with open("parkoviste.csv", encoding="utf-8", newline='') as f, \
-	open("pr.csv","w",encoding="utf-8", newline='') as fout:
-	reader = csv.reader(f, delimiter=";")
-	next(reader)
-	# Vytvoříme si zapisovací objekt pro CSV nad výstpním souborem
-	writer = csv.writer(fout)
-	for row in reader:
-		# Pokud sloupec pr neobsahuje `true` libovolně velkými písmeny, řádek přeskočíme
-		if row[3].lower() != 'true':
-			continue
-		#writer.writerow([row[0],row[-1]]) # Ekvivalentní s řádky níže
-		# Z načteného řádku vyextrahujeme jméno a kapacitu
-		name = row[0]
-		capa = row[-1]
-		# Vytvoříme si výstupní řádek jako seznam sloupců
-		outrow = [name, capa]
-		# Zapíšeme řádek do výstupního souboru
-		writer.writerow(outrow)"""
+except FileNotFoundError:                                               
+	print("Soubor není.")
+except: 
+	print("Něco se šíleně pokazilo.")
