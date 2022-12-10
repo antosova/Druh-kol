92e87fddbd2021f020c203dcf88951d0e188f500
Kristýna Antošová <antosovkr@natur.cuni.cz>
2022-12-10 15:42:34 +0100

    2-3 commit,sobota

--- 2_du.py
+++ 2_du.py
@@ -1,17 +1,15 @@
 import csv
 
-# Otevřeme a přečteme soubor Data_prutok.csv
-"""with open("Data_prutok.csv", encoding="utf-8", newline='') as f:
+"""# Otevřeme a přečteme soubor Data_prutok.csv
+with open("Data_prutok.csv", encoding="utf-8", newline='') as f:
 	reader = csv.reader(f, delimiter=",")
 	for row in reader:
 		print(row)
-
 with open("Data_prutok.csv", encoding="utf-8", newline='') as f:
 	reader = csv.reader(f, delimiter=",")
 	for row in range(7):
 		(prutoky) = (next(reader)[3])
 		print (prutoky)
-
 prutoky_list = []
 prutoky_list.append(prutoky)
 print(prutoky_list)
@@ -19,22 +17,33 @@ print(prutoky_list)
 
 suma_tyden = 0
 suma_rok = 0
-index = 1
+pocitadlo_radku = 1
+rok = 1
 with open("Data_prutok.csv", encoding="utf-8", newline='') as f:
 	reader = csv.reader(f, delimiter=",")
 	for row in reader:
-		suma_tyden+= row[3]
-		suma_rok+= row[3]
+		suma_tyden += float(row[3])
+		if pocitadlo_radku % 7 == 0:
+			suma_tyden = round(suma_tyden/7,4)
+			print (suma_tyden)
+			suma_tyden = 0
+		pocitadlo_radku+=1
 
-		if index % 7 == 0:
-			print (suma_tyden/7)
-			suma_tyden = 0
-		
-		if index % 365:
-			print (suma_rok/365)
-			suma_tyden = 0
-		index+=1
-		
-#druhej commit
-			
+print("průmery za rok:")
+with open("Data_prutok.csv", encoding="utf-8", newline='') as f:
+	reader = csv.reader(f, delimiter=",")
+	for row in reader:
+		suma_rok += float(row[3])
+		if rok % 4 == 1:
+			if pocitadlo_radku % 366 == 0:
+				suma_rok = round(suma_rok/366,4)
+				print (suma_rok)
+				suma_rok = 0
+				rok+=1
+		if pocitadlo_radku % 365 == 0:
+				suma_rok = round(suma_rok/365,4)
+				print (suma_rok)
+				suma_rok = 0
+				rok+=1
+		pocitadlo_radku+=1
    
